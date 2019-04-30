585859be5284c61fa81164539df8cf3c2c375d45
ltecot <lucastecot@gmail.com>
2019-04-29 23:38:59 -0700

    full bounds working

--- pygcn/test_robustness.py
+++ pygcn/test_robustness.py
@@ -11,7 +11,7 @@ import torch.optim as optim
 
 from pygcn.utils import load_data, accuracy
 # from pygcn.models import GCN
-from pygcn.robustness import GCNBounds
+from pygcn.robustness import GCNBoundsRelaxed, GCNBoundsFull
 
 np.random.seed(42)
 torch.manual_seed(42)
@@ -21,13 +21,34 @@ adj, features, labels, idx_train, idx_val, idx_test = load_data()
 adj = adj.to_dense()  # Temporarily to have less headaches. Also note that each layer must have an adj, which is an issue.
 
 # Model and optimizer
-model = torch.load('gcn_model.pth')
+# model = torch.load('gcn_model.pth')
+# model = torch.load('gcn_model_small.pth')
+relaxed = True
+small = True
+eps = 0.001
+
+if small:
+    model = torch.load('gcn_model_small.pth')
+    adj = adj[0:100, 0:100]
+    features = features[:100]
+    labels = labels[:100]
+else:
+    model = torch.load('gcn_model.pth')
 
 # Bounds
-bound_calc = GCNBounds(model, features, adj, 0.01)
-bounds = bound_calc.bounds
-print(bounds)
-torch.save({
-            'lower_bound': bounds[0],
-            'upper_bound': bounds[1],
-            }, 'test_bounds.pt')
+if relaxed:
+    bound_calc = GCNBoundsRelaxed(model, features, adj, eps)
+    bounds = bound_calc.bounds
+    print(bounds)
+    torch.save({
+                'lower_bound': bounds[0],
+                'upper_bound': bounds[1],
+                }, 'test_bounds_relaxed.pt')
+else: # full
+    bound_calc = GCNBoundsFull(model, features, adj, eps)
+    bounds = bound_calc.bounds
+    print(bounds)
+    torch.save({
+                'lower_bound': bounds[0],
+                'upper_bound': bounds[1],
+                }, 'test_bounds_full.pt')
--- pygcn/train.py
+++ pygcn/train.py
@@ -29,6 +29,8 @@ parser.add_argument('--hidden', type=int, default=16,
                     help='Number of hidden units.')
 parser.add_argument('--dropout', type=float, default=0.5,
                     help='Dropout rate (1 - keep probability).')
+parser.add_argument('--small', action='store_true', default=True, # default=False,
+                    help='Train a small model instead')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
@@ -41,6 +43,20 @@ if args.cuda:
 # Load data
 adj, features, labels, idx_train, idx_val, idx_test = load_data()
 
+# print(idx_train)
+# print(idx_val)
+if args.small:
+    adj = adj.to_dense()
+    adj = adj[0:100, 0:100]
+    # print(adj.shape)
+    features = features[:100]
+    labels = labels[:100]
+    idx_train = torch.LongTensor(range(70))
+    idx_val = torch.LongTensor(range(70, 90))
+    idx_test = torch.LongTensor(range(90, 100))
+else:
+    adj = adj.to_dense()  # To make life easier for now
+    # idx_train = 
 # if args.cuda:
 #     # model.cuda()
 #     features = features.cuda()
@@ -62,7 +78,7 @@ model = gcn_sequential_model(nfeat=features.shape[1],
 optimizer = optim.Adam(model.parameters(),
                        lr=args.lr, weight_decay=args.weight_decay)
 
-print(features.shape[1], args.hidden, labels.max().item() + 1, features.shape)
+# print(features.shape[1], args.hidden, labels.max().item() + 1, features.shape)
 
 # if args.cuda:
 #     model.cuda()
@@ -113,7 +129,10 @@ def test():
 t_total = time.time()
 for epoch in range(args.epochs):
     train(epoch)
-torch.save(model, "gcn_model.pth")
+if args.small:
+    torch.save(model, "gcn_model_small.pth")
+else:
+    torch.save(model, "gcn_model.pth")
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
