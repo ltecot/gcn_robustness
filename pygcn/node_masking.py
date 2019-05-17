bfd77c7c9837801a119b7cdf1e3298131415ccb4
Huan Zhang and Minhao Cheng <huan@huan-zhang.com>
2019-05-17 16:05:26 -0700

    updated

--- pygcn/node_masking.py
+++ pygcn/node_masking.py
@@ -1,18 +1,23 @@
 import torch
 
 
-def select_target_node(adj, n_neighbours, pred, label):
-    mask = (pred==label)
-    #print(mask)
+def select_target_node(adj, n_neighbours, pred, label, idx_test):
+    #print(idx_test)
+    adj = adj[idx_test]
+    mask = (pred==label[idx_test])
+    #print(pred.size(0))
     adj_filter = adj[mask]
+    #print(len(idx_test))
+    #print(adj_filter.size(0))
     #nnz = torch.nonzero(adj_filter)
     #print(nnz.size())
     n_idx = []
     for i in range(adj_filter.size(0)):
         nnz = torch.nonzero(adj_filter[i])
         if nnz.size(0)> n_neighbours:
-            n_idx.append(i)
-    return n_idx
+            n_idx.append(idx_test[mask][i].item())
+    #print(n_idx)
+    return sorted(n_idx)
 
 def select_perturb_node(adj,target_node,hops, random_p, with_target_node):
     #adj = adj - torch.eye(adj.size(0))
@@ -32,7 +37,8 @@ def select_perturb_node(adj,target_node,hops, random_p, with_target_node):
         perm = torch.randperm(adj.size(0))
         k = int(adj.size(0)*random_p)
         if with_target_node:
-            perm = perm[perm!=target_node]
+            for tn in target_node:
+                perm = perm[perm!=tn]
         n_idx = torch.sort(perm[:k])[0]
     return n_idx
         
