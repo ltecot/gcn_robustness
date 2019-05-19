1adaad1115a85885417ec8c43ef73fbcedb8d031
Huan Zhang and Minhao Cheng <huan@huan-zhang.com>
2019-05-19 14:09:38 -0700

    bound problem

---
+++ pygcn/test_attack.py
@@ -0,0 +1,173 @@
+import sys
+import time
+import random
+import argparse
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import h5py
+from utils import load_data, accuracy
+from models import gcn_sequential_model
+from PGD import PGD
+from node_masking import select_target_node, select_perturb_node, select_target_node_sp, select_perturb_node_sp
+import pickle
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='Disables CUDA training.')
+parser.add_argument('--fastmode', action='store_true', default=False,
+                    help='Validate during training pass.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='Number of epochs to train.')
+parser.add_argument('--lr', type=float, default=0.01,
+                    help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=5e-4,
+                    help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16,
+                    help='Number of hidden units.')
+parser.add_argument('--dropout', type=float, default=0.5,
+                    help='Dropout rate (1 - keep probability).')
+parser.add_argument('--dataset', type=str, default="cora",
+                    help='Dataset to be used')
+parser.add_argument('--p_file', type=str, default=None,
+                    help='Pickle file to be used')
+
+parser.add_argument('--n_neigh', type=int, default=0,
+                    help='number of neighbors of target node')
+parser.add_argument('--start', type=int, default=0,
+                    help='starting node')
+parser.add_argument('--npoints', type=int, default=10,
+                    help='points to be added')
+parser.add_argument('--hops', type=int, default=1,
+                    help='hops of neighbors of target node')
+parser.add_argument('--epsilon', type=float, default=0.01,
+                    help='epsilon in the PGD attack')
+parser.add_argument('--verbose', action='store_true', default=False,
+                    help='verbose.')
+
+
+args = parser.parse_args()
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+
+
+def test(model, features, labels, idx_test):
+    #model.eval()
+    # output = model(features, adj)
+    output = model(features)
+    #print(output)
+    #print(labels)
+    # loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    loss_test = F.nll_loss(F.log_softmax(output[idx_test], dim=1), labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
+   
+    preds = output[idx_test].max(1)[1].type_as(labels[idx_test])
+    #print(output[idx_test].size(), len(idx_test))
+    return preds, acc_test 
+
+
+
+
+if args.dataset == "reddit":
+    num_data, train_adj, full_adj, features, train_features, test_features, labels, train_d, val_d, test_d, adj_t = \
+            load_data(args.dataset)
+    print(features.shape,labels.shape)
+    model = gcn_sequential_model(nfeat=features.shape[1],
+                                 nhid=int(128), 
+                                 nclass=41,
+                                 adj=adj_t)
+    model.eval()
+    weights={}
+    keys = []
+    with h5py.File("reddit_gcn_model.hdf5","r") as hf:
+        hf.visit(keys.append)
+        for key in keys:
+            if ':' in key:
+                print(hf[key].name)
+                weights[hf[key].name] = hf[key].value
+
+    hf.close()
+    #print(weights)
+    model[0].weight.data.copy_(torch.from_numpy(weights["/model/dense0_vars/weights:0"])) 
+    model[1].weight.data.copy_(torch.from_numpy(weights["/model/dense1_vars/weights:0"])) 
+    features= torch.FloatTensor(features)
+    #print(labels)
+    labels = torch.LongTensor(labels)
+    preds, clean_acc = test(model, features, labels, test_d)
+    #Test()
+    #adj = adj_t
+
+else:
+    adj, features, labels, idx_train, idx_val, idx_test = load_data(args.dataset)
+    #print(len(idx_test)) 
+    #print(features.size())
+    adj = adj.to_dense()
+    #print(torch.nonzero(adj!=adj.t()))
+    model = gcn_sequential_model(nfeat=features.shape[1],
+                                 nhid=args.hidden, 
+                                 nclass=labels.max().item() + 1,
+                                 adj=adj)
+    model_path = args.dataset+"_"+str(args.hidden)+"_gcn_model.pth"
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    preds, clean_acc = test(model, features, labels, idx_test)
+ 
+
+
+starting_point = args.start
+npoints = args.npoints
+end_point = starting_point + npoints
+point_list = pickle.load(open(args.p_file,"rb"))
+np.random.shuffle(point_list)
+target_list = point_list[starting_point:end_point]
+attack = PGD(model)
+epsilon = args.epsilon
+#correct = 0
+dir_name = "logs/"+args.dataset+"_"+str(epsilon)+"_"+str(args.hops)
+os.makedirs(dir_name, exist_ok=True)
+filename = dir_name+"/"+str(starting_point)+".log"
+log_f = open(filename,"w")
+
+
+for target_idx in target_list:
+    print("Attacking on "+str(target_idx))
+    target_idx = [target_idx]
+    hops = args.hops
+    #perturb_idx = select_perturb_node(adj, target_idx, hops, None, False)
+    #perturb_idx = select_perturb_node(adj, target_idx, hops, None, True)
+    #perm =  torch.randperm(perturb_idx.size(0))
+    perturb_idx = torch.LongTensor(target_idx)
+    if args.dataset=="reddit":
+        #perturb_idx = select_perturb_node_sp(full_adj, target_idx, hops, None, False)
+    
+        print("Perturbing on "+ str(len(perturb_idx)/features.shape[0]*100)+" nodes")
+    else:
+        perturb_idx = select_perturb_node(adj, target_idx, hops, None, True)
+        #perturb_idx = perturb_idx[perm[:20]]
+        #perturb_idx = torch.LongTensor(target_idx)
+        print("Perturbing on "+ str(perturb_idx.size(0)/features.size(0)*100)+" nodes")
+    #print(perturb_idx)
+    mask = torch.FloatTensor(features.size())
+    mask.zero_()
+    mask[perturb_idx,:] = 1
+    features_adv = attack(features, labels, target_idx, mask, epsilon)
+    _, acc= test(model, features_adv, labels, target_idx)
+    if acc==0:
+        log_f.write("success!\n")
+    else:
+        log_f.write("fail!\n")
+
+log_f.close()
+#print('job start, attack point {} to {}'.format(starting_point, end_point))
+#t = 1 + random.randint(0,10)
+#print('job starting at point {} will run {} seconds'.format(starting_point, t))
+#time.sleep(t)
+#print('job starting at point {} done'.format(starting_point))
